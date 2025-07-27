83f2261e543c96fbbcf3519cce57283516681200
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-07-27 11:56:18 +0500

    add logo and install ruff

--- proxi/main.py
+++ proxi/main.py
@@ -1,6 +1,9 @@
 import sys
 
-from PySide6 import QtWidgets
+from PySide6 import QtSvgWidgets, QtWidgets
+
+WINDOW_WIDTH = 460
+WINDOW_HEIGHT = 700
 
 
 class MainWidget(QtWidgets.QWidget):
@@ -9,8 +12,15 @@ class MainWidget(QtWidgets.QWidget):
 
         self.button = QtWidgets.QPushButton("Set proxy")
 
+        self.logo = QtSvgWidgets.QSvgWidget("./assets/logo-light-theme.svg")
+        self.logo.setFixedSize(96, 56)
+
         self.box_layout = QtWidgets.QVBoxLayout(self)
+        self.box_layout.addWidget(self.logo)
         self.box_layout.addWidget(self.button)
+        self.box_layout.addStretch(0)
+        self.box_layout.setSpacing(20)
+
         self.setLayout(self.box_layout)
 
 
@@ -18,7 +28,7 @@ def main():
     app = QtWidgets.QApplication([])
 
     main_widget = MainWidget()
-    main_widget.resize(800, 600)
+    main_widget.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
     main_widget.show()
 
     sys.exit(app.exec())
