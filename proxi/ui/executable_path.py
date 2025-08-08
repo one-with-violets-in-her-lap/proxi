b5926cf32f599e1e53bbb9c28ebcb5e10c095edb
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-08-08 08:01:24 +0500

    ui/executable_path: fix unknown attr

--- proxi/ui/executable_path.py
+++ proxi/ui/executable_path.py
@@ -5,7 +5,7 @@ if getattr(sys, "frozen", False):
     # If the application is run as a bundle, the PyInstaller bootloader
     # extends the sys module by a flag frozen=True and sets the app
     # path into variable _MEIPASS'.
-    application_path = sys._MEIPASS
+    application_path = getattr(sys, "_MEIPASS")
 else:
     application_path = os.path.abspath("./")
 
