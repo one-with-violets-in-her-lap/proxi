44d83404958f6ad833fa521080475b67cef991df
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-08-08 10:56:33 +0500

    platform detection: prioritize kde over gsettings

--- proxi/core/utils/platform.py
+++ proxi/core/utils/platform.py
@@ -13,17 +13,17 @@ _logger = logging.getLogger(__name__)
 
 
 def get_user_settings_platform():
-    gsettings_bin_path = shutil.which("gsettings")
-
-    if gsettings_bin_path is not None:
-        _logger.info("GSettings tool detected")
-        return SettingsPlatform.GSETTINGS
-
     kreadconfig6_path = shutil.which("kreadconfig6")
 
     if kreadconfig6_path is not None:
         _logger.info("KDE 6 config tool detected")
         return SettingsPlatform.KDE_6_CONFIG
+
+    gsettings_bin_path = shutil.which("gsettings")
+
+    if gsettings_bin_path is not None:
+        _logger.info("GSettings tool detected")
+        return SettingsPlatform.GSETTINGS
 
     _logger.info(
         "No support settings utility found, falling back to basic shell environment variables"
