60c1c0fa8f3a5ca145097408de4155e54cd7ee89
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-08-22 15:39:00 +0500

    cli/proxy switch: colorize outputs

--- proxi/cli/commands/proxy_switch.py
+++ proxi/cli/commands/proxy_switch.py
@@ -4,6 +4,9 @@ import click
 
 from proxi.cli.context import CliContext
 
+ENABLED_MESSAGE = click.style("enabled 🗸", fg="green")
+DISABLED_MESSAGE = click.style("disabled", fg="red")
+
 
 @click.command("status")
 @click.pass_context
@@ -17,7 +20,7 @@ def handle_proxy_status_command(context: click.Context):
     milliseconds_took = (timeit.default_timer() - start_seconds_time) * 1000
 
     click.echo(
-        f"Proxy is {'enabled' if is_proxy_active else 'disabled'}"
+        f"Proxy is {ENABLED_MESSAGE if is_proxy_active else DISABLED_MESSAGE}"
         + click.style(f"\t({milliseconds_took:.1f}ms)", dim=True)
     )
 
@@ -33,7 +36,10 @@ def handle_proxy_on_command(context: click.Context):
 
     milliseconds_took = (timeit.default_timer() - start_seconds_time) * 1000
 
-    click.echo("Proxy is enabled" + click.style(f"\t({milliseconds_took:.1f}ms)", dim=True))
+    click.echo(
+        f"Proxy is {ENABLED_MESSAGE}"
+        + click.style(f"\t({milliseconds_took:.1f}ms)", dim=True)
+    )
 
 
 @click.command("off")
@@ -47,4 +53,7 @@ def handle_proxy_off_command(context: click.Context):
 
     milliseconds_took = (timeit.default_timer() - start_seconds_time) * 1000
 
-    click.echo("Proxy is disabled" + click.style(f"\t({milliseconds_took:.1f}ms)", dim=True))
+    click.echo(
+        f"Proxy is {DISABLED_MESSAGE}"
+        + click.style(f"\t({milliseconds_took:.1f}ms)", dim=True)
+    )
